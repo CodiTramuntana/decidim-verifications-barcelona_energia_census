10cab9f07e9263ee72189f9525e11c23019c5755
Isaac Massot <isaac.mg@coditramuntana.com>
2019-01-11 11:24:23 +0100

    remove unnecessary admin_engine and admin.rb files

--- lib/decidim/verifications/barcelona_energia_census.rb
+++ lib/decidim/verifications/barcelona_energia_census.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'decidim/verifications/barcelona_energia_census/admin'
 require 'decidim/verifications/barcelona_energia_census/engine'
-require 'decidim/verifications/barcelona_energia_census/admin_engine'
 require 'decidim/verifications/barcelona_energia_census/workflow'
 
 module Decidim
